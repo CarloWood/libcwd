15d73cb4749012064d1095d46be2ab575627e413
libcw <libcw@71f70d73-0222-0410-9cd5-a531ff9bb54e>
2000-07-21 09:44:26 +0000

    Got rid of <strstream> in debug.h

---
+++ include/libcw/no_alloc_checking_ostrstream.h
@@ -0,0 +1,48 @@
+// $Header$
+//
+// Copyright (C) 2000, by
+//
+// Carlo Wood, Run on IRC <carlo@alinoe.com>
+// RSA-1024 0x624ACAD5 1997-01-26                    Sign & Encrypt
+// Fingerprint16 = 32 EC A7 B6 AC DB 65 A6  F6 F6 55 DD 1C DC FF 61
+//
+// This file may be distributed under the terms of the Q Public License
+// version 1.0 as appearing in the file LICENSE.QPL included in the
+// packaging of this file.
+//
+
+#ifndef LIBCW_NO_ALLOC_CHECKING_OSTRSTREAM_H
+#define LIBCW_NO_ALLOC_CHECKING_OSTRSTREAM_H
+
+RCSTAG_H(no_alloc_checking_ostrstream, "$Id$")
+
+#include <strstream>
+
+#ifndef DEBUGNONAMESPACE
+namespace libcw {
+  namespace debug {
+#endif
+
+#ifdef DEBUGMALLOC
+
+    class no_alloc_checking_ostrstream : public ostrstream {
+    private:
+      strstreambuf* my_sb;
+    public:
+      no_alloc_checking_ostrstream(void);
+      ~no_alloc_checking_ostrstream();
+      strstreambuf* rdbuf() { return my_sb; }
+    };
+
+#else // !DEBUGMALLOC
+
+    typedef ostrstream no_alloc_checking_ostrstream;
+
+#endif // !DEBUGMALLOC
+
+#ifndef DEBUGNONAMESPACE
+  }; // namespace debug
+}; // namespace libcw
+#endif
+
+#endif // LIBCW_NO_ALLOC_CHECKING_OSTRSTREAM_H
