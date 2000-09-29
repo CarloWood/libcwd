f234c2be85a6cd4d08d68a40d9f9c6844d359f20
libcw <libcw@71f70d73-0222-0410-9cd5-a531ff9bb54e>
2000-09-29 19:11:53 +0000

    Work in progress on testsuite.

--- tests/basic.cc
+++ tests/basic.cc
@@ -7,7 +7,11 @@ int main(void)
   Debug( libcw_do.on() );
   Debug( dc::notice.on() );
 
-  Dout(dc::notice, "Basic Test");
+  int i = 1;
+  Dout(dc::notice, "Basic Test.");
+  Dout(dc::warning, "This should not be printed." << ++i);
+  Debug( dc::warning.on() );
+  Dout(dc::warning, "This should be a one: " << i);
 
   exit(0);
 }
