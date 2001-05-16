d06fcfef30e479e8add7178e05f0ac29c2baa5db
libcw <libcw@71f70d73-0222-0410-9cd5-a531ff9bb54e>
2001-05-16 01:42:25 +0000

    New little test.

---
+++ tests/dout_alloc.cc
@@ -0,0 +1,39 @@
+#include <libcw/sys.h>
+#include <libcw/debug.h>
+
+int main(void)
+{
+  //----------------------------------------------------------------------
+  // The following calls will be done in almost every program using libcwd
+ 
+  Debug( check_configuration() );
+ 
+#ifdef DEBUGMALLOC
+  // Don't show allocations that are allocated before main()
+  libcw::debug::make_all_allocations_invisible_except(NULL);
+#endif
+ 
+  // Select channels (note that where 'on' is used, 'off' can be specified
+  // and vica versa).
+  ForAllDebugChannels( if (!debugChannel.is_on()) debugChannel.on(); );
+ 
+  // Write debug output to cout (the default is cerr)
+  Debug( libcw_do.set_ostream(&std::cout) );
+ 
+  // Turn debug object on.  Turning it off can be done recursively.
+  // It starts with 'off' depth 1.
+  Debug( libcw_do.on() );
+ 
+  // List all debug channels (not very usefull unless you allow to turn
+  // channels on and off from the commandline; this is supported in libcw).
+  Debug( list_channels_on(libcw_do) );
+ 
+  // End of common block
+  //----------------------------------------------------------------------
+
+  int* p;
+  Dout(dc::notice, "Allocation inside Dout: " << (p = new int [10]));
+  delete p;
+
+  return 0;
+}
