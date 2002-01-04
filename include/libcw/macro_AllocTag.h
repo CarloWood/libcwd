ef22f9925041c1530807f05fcb909b8ef939cea4
libcw <libcw@71f70d73-0222-0410-9cd5-a531ff9bb54e>
2002-01-04 04:43:38 +0000

    Kludge to work around g++ bug for FreeBSD.

--- include/libcw/macro_AllocTag.h
+++ include/libcw/macro_AllocTag.h
@@ -204,7 +204,13 @@ extern void register_external_allocation(void const* ptr, size_t size);
     } while(0)
 
 template<typename TYPE>
+#ifndef __FreeBSD__
+  // There is a bug in g++ that causes the wrong line number information to be generated
+  // when this function is inline.  I was able to use heuristics to work around that for
+  // STABS, but not for DWARF-2 (the default of FreeBSD).
+  // See http://gcc.gnu.org/cgi-bin/gnatsweb.pl?cmd=view%20audit-trail&database=gcc&pr=5271
   __inline__
+#endif
   TYPE*
   __libcwd_allocCatcher(TYPE* new_ptr)
   {
