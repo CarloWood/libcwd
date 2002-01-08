664beb6a3fa3d8e13459f424d6cfd038535a383c
libcw <libcw@71f70d73-0222-0410-9cd5-a531ff9bb54e>
2002-01-08 23:46:52 +0000

    Ugh - forgot to add this file (long time ago).

---
+++ nodebug.h
@@ -0,0 +1,13 @@
+#define AllocTag1(p)
+#define AllocTag2(p, desc)
+#define AllocTag_dynamic_description(p, x)
+#define AllocTag(p, x)
+#define Debug(x)
+#define Dout(a, b)
+#define DoutFatal(a, b) LibcwDoutFatal(::std, , a, b)
+#define ForAllDebugChannels(STATEMENT)
+#define ForAllDebugObjects(STATEMENT)
+#define LibcwDebug(dc_namespace, x)
+#define LibcwDout(a, b, c, d)
+#define LibcwDoutFatal(a, b, c, d) do { ::std::cerr << d << ::std::endl; ::std::exit(254); } while(1)
+#define NEW(x) new x
