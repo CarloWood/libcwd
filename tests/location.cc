0d7d888846d7a7304a97cad7301a53ffc8d9af8d
libcw <libcw@71f70d73-0222-0410-9cd5-a531ff9bb54e>
2002-02-21 20:18:37 +0000

    Just another tiny test.

---
+++ tests/location.cc
@@ -0,0 +1,47 @@
+// $Header$
+//
+// Copyright (C) 2002, by
+// 
+// Carlo Wood, Run on IRC <carlo@alinoe.com>
+// RSA-1024 0x624ACAD5 1997-01-26                    Sign & Encrypt
+// Fingerprint16 = 32 EC A7 B6 AC DB 65 A6  F6 F6 55 DD 1C DC FF 61
+//
+// This file may be distributed under the terms of the Q Public License
+// version 1.0 as appearing in the file LICENSE.QPL included in the
+// packaging of this file.
+//
+
+#include "sys.h"
+#include <iostream>
+#include "debug.h"
+#include <libcw/demangle.h>
+
+void test(void)
+{
+  libcw::debug::location_ct loc((char*)__builtin_return_address(0) + libcw::debug::builtin_return_address_offset);
+  std::string funcname;
+  libcw::debug::demangle_symbol(loc.mangled_function_name(), funcname);
+  Dout(dc::notice, "Called from " << funcname );
+}
+
+class A {
+  public:
+    A(void);
+};
+
+A::A(void)
+{
+  Dout(dc::notice, "Called from " << location_ct((char*)__builtin_return_address(0) + libcw::debug::builtin_return_address_offset) );
+  test();
+}
+
+int main(void)
+{
+  ForAllDebugChannels( if (!debugChannel.is_on()) debugChannel.on(); );
+  Debug( libcw_do.set_ostream(&std::cout) );
+  Debug( libcw_do.on() );
+
+  delete NEW(A);
+
+  return 0;
+}
