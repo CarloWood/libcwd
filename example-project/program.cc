b6b273701c8c9b5530a6cfcfd526de23c8ad7efb
libcw <libcw@71f70d73-0222-0410-9cd5-a531ff9bb54e>
2002-02-21 18:22:01 +0000

    Update of the example project.

--- example-project/program.cc
+++ example-project/program.cc
@@ -1,12 +1,25 @@
 #include "sys.h"
-#include <iostream>
+#include <iostream>		// Needed for std::cout and std::endl
+#ifdef CWDEBUG
+#include <cstdlib>		// Needed for getenv
+#endif
 #include "debug.h"
 
 int main(void)
 {
+  // You want this, unless you mix streams output with C output.
+  // Read  http://gcc.gnu.org/onlinedocs/libstdc++/27_io/howto.html#8 for an explanation.
+  std::ios::sync_with_stdio(false);
+
+  // This will warn you when you are using header files that do not belong to the
+  // shared libcwd object that you linked with.
   Debug( check_configuration() );
+
+  // Turn on the custom channel.
   Debug( dc::custom.on() );
-  Debug( libcw_do.on() );
+
+  // Only turn on debug output when the environment variable SUPPRESS_DEBUG_OUTPUT is not set.
+  Debug( if (getenv("SUPPRESS_DEBUG_OUTPUT") == NULL) libcw_do.on() );
 
   Dout(dc::custom, "This is debug output, written to a custom channel (see ./debug.h and ./debug.cc)");
 
