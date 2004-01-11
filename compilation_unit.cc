34efaf9b2de64529ffa6fdf7c50b32a3d090cd8a
libcw <libcw@71f70d73-0222-0410-9cd5-a531ff9bb54e>
2004-01-11 17:12:35 +0000

    New file

---
+++ compilation_unit.cc
@@ -0,0 +1,25 @@
+// $Header$
+//
+// Copyright (C) 2003, by
+// 
+// Carlo Wood, Run on IRC <carlo@alinoe.com>
+// RSA-1024 0x624ACAD5 1997-01-26                    Sign & Encrypt
+// Fingerprint16 = 32 EC A7 B6 AC DB 65 A6  F6 F6 55 DD 1C DC FF 61
+//
+// This file may be distributed under the terms of the Q Public License
+// version 1.0 as appearing in the file LICENSE.QPL included in the
+// packaging of this file.
+//
+
+#include "sys.h"
+#include <libcwd/config.h>
+#include "cwd_debug.h"
+#include "compilation_unit.h"
+
+namespace libcw {
+  namespace debug {
+    namespace _private_ {
+
+    } // namespace _private_
+  } // namespace debug
+} // namespace libcw
