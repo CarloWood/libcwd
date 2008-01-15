c5d7047bef6b0fc04798be13d46b08048a8a1a9b
libcw <libcw@71f70d73-0222-0410-9cd5-a531ff9bb54e>
2008-01-15 22:37:41 +0000

    Fixed typo: ELFDATAMSB -> ELFDATA2MSB

--- elfxx.cc
+++ elfxx.cc
@@ -1382,13 +1382,13 @@ static bool check_elf_format(Elfxx_Ehdr const& header)
 #if __BYTE_ORDER == __LITTLE_ENDIAN
       ELFDATA2LSB
 #elif __BYTE_ORDER == __BIG_ENDIAN
-      ELFDATAMSB
+      ELFDATA2MSB
 #else
       ELFDATANONE
 #endif
 #else // !__BYTE_ORDER
 #ifdef WORDS_BIGENDIAN
-      ELFDATAMSB
+      ELFDATA2MSB
 #else
       ELFDATA2LSB
 #endif
