b2e4b8dc1963a32583940fd9205a537cf636db87
Carlo Wood <carlo@alinoe.com>
2014-03-13 16:43:44 +0100

    Fix autogen.sh to test for .git directory.

--- autogen.sh
+++ autogen.sh
@@ -1,8 +1,8 @@
 #! /bin/sh
 
 # Clueless user check.
-if test ! -d CVS -a ! -d .svn -a -f configure; then
-  echo "You only need to run './autogen.sh' when you checked out this project using svn."
+if test ! -d .git -a -f configure; then
+  echo "You only need to run './autogen.sh' when you checked out this project using git."
   echo "Just run ./configure [--help]."
   echo "If you insist on running it, then first remove the 'configure' script."
   exit 0
