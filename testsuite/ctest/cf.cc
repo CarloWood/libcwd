36f26e5d4ec342504933afd1403d4d967952ca76
Carlo Wood <carlo@alinoe.com>
2026-05-30 21:28:37 +0200

    Port cf.cc and demangler.cc to ctest.

---
+++ testsuite/ctest/cf.cc
@@ -0,0 +1,106 @@
+// Verify libcwd control flags for prefixes, labels, markers, newlines, errors, and cerr routing.
+//
+// This is the CTest-backed form of testsuite/libcwd.tst/cf.cc. It writes the
+// same sequence of control-flag combinations to a captured debug stream and
+// checks the exact visible output, including the line formed by several
+// nonewline_cf writes and the cerr_cf line emitted by a separate debug object.
+
+#include "cwd_sys.h"
+#include "test_support.h"
+#include "pending_stream.h"
+
+#include <cerrno>
+#include <cstdlib>
+#include <sstream>
+
+libcwd::debug_ct local_debug_object;
+#define MyDout(cntrl, data) LibcwDout(LIBCWD_DEBUGCHANNELS, local_debug_object, cntrl, data)
+
+int main()
+{
+  libcwd_ctest::PendingStream captured;
+  std::ostringstream hidden_local_output;
+
+  Debug(main_reached());
+  Debug(libcw_do.set_ostream(&captured));
+  Debug(local_debug_object.set_ostream(&hidden_local_output));
+  Debug(local_debug_object.on());
+  Debug(libcw_do.on());
+  Debug(dc::notice.on());
+
+  Debug(libcw_do.margin().assign("MARGIN", 6));
+  Debug(libcw_do.marker().assign("MARKER", 6));
+  Debug(libcw_do.set_indent(3));
+  Debug(local_debug_object.margin().assign("MARGIN", 6));
+  Debug(local_debug_object.marker().assign("MARKER", 6));
+  Debug(local_debug_object.set_indent(3));
+
+  {
+    libcwd_ctest::redirect_cerr_ct redirect(captured);
+
+    Dout(dc::notice|nonewline_cf, "x");
+    Dout(dc::notice|nonewline_cf, "y");
+    Dout(dc::notice, "z<newline>");
+
+    Dout(dc::notice, "<no flags>");
+    Dout(dc::notice|noprefix_cf, "noprefix_cf");
+    Dout(dc::notice|nolabel_cf, "nolabel_cf");
+    Dout(dc::notice|blank_margin_cf, "blank_margin_cf");
+    Dout(dc::notice|blank_label_cf, "blank_label_cf");
+    Dout(dc::notice|blank_marker_cf, "blank_marker_cf");
+
+    Dout(dc::notice|noprefix_cf|nonewline_cf, "a");
+    Dout(dc::notice|nolabel_cf|nonewline_cf, "b");
+    Dout(dc::notice|blank_margin_cf|nonewline_cf, "c");
+    Dout(dc::notice|blank_label_cf|nonewline_cf, "d");
+    Dout(dc::notice|blank_marker_cf|nonewline_cf, "e");
+    Dout(dc::notice, "f");
+
+    Dout(dc::notice|nolabel_cf|noprefix_cf, "nolabel_cf|noprefix_cf");
+    Dout(dc::notice|blank_margin_cf|noprefix_cf, "blank_margin_cf|noprefix_cf");
+    Dout(dc::notice|blank_label_cf|noprefix_cf, "blank_label_cf|noprefix_cf");
+    Dout(dc::notice|blank_marker_cf|noprefix_cf, "blank_marker_cf|noprefix_cf");
+
+    Dout(dc::notice|blank_margin_cf|nolabel_cf, "blank_margin_cf|nolabel_cf");
+    Dout(dc::notice|blank_label_cf|nolabel_cf, "blank_label_cf|nolabel_cf");
+    Dout(dc::notice|blank_marker_cf|nolabel_cf, "blank_marker_cf|nolabel_cf");
+
+    Dout(dc::notice|blank_label_cf|blank_margin_cf, "blank_label_cf|blank_margin_cf");
+    Dout(dc::notice|blank_marker_cf|blank_margin_cf, "blank_marker_cf|blank_margin_cf");
+    Dout(dc::notice|blank_marker_cf|blank_label_cf, "blank_marker_cf|blank_label_cf");
+
+    errno = EAGAIN;
+    Dout(dc::notice|error_cf, "error_cf");
+
+    MyDout(dc::notice, "This is written to buf");
+    MyDout(dc::notice|cerr_cf, "cerr_cf");
+  }
+
+  captured.rdbuf()->pubsync();
+
+  char const* expected[] = {
+    "MARGINNOTICE  MARKER   xMARGINNOTICE  MARKER   yMARGINNOTICE  MARKER   z<newline>",
+    "MARGINNOTICE  MARKER   <no flags>",
+    "noprefix_cf",
+    "MARGINnolabel_cf",
+    "      NOTICE  MARKER   blank_margin_cf",
+    "MARGIN        MARKER   blank_label_cf",
+    "MARGINNOTICE           blank_marker_cf",
+    "aMARGINb      NOTICE  MARKER   cMARGIN        MARKER   dMARGINNOTICE           eMARGINNOTICE  MARKER   f",
+    "nolabel_cf|noprefix_cf",
+    "blank_margin_cf|noprefix_cf",
+    "blank_label_cf|noprefix_cf",
+    "blank_marker_cf|noprefix_cf",
+    "      blank_margin_cf|nolabel_cf",
+    "MARGINblank_label_cf|nolabel_cf",
+    "MARGINblank_marker_cf|nolabel_cf",
+    "              MARKER   blank_label_cf|blank_margin_cf",
+    "      NOTICE           blank_marker_cf|blank_margin_cf",
+    "MARGIN                 blank_marker_cf|blank_label_cf",
+    "MARGINNOTICE  MARKER   error_cf: EAGAIN (Resource temporarily unavailable)",
+    "MARGINNOTICE  MARKER   cerr_cf",
+    nullptr
+  };
+
+  return libcwd_ctest::matches_expected_output(captured.input(), expected) ? EXIT_SUCCESS : EXIT_FAILURE;
+}
---
+++ testsuite/ctest/demangler.cc
@@ -0,0 +1,103 @@
+// Verify libcwd::demangle_symbol against the legacy demangler regression cases.
+//
+// The test feeds a list of curated mangled names through libcwd::demangle_symbol
+// and exact-matches the current maintained demangled spelling for each case.
+// It has no side effects beyond diagnostics on mismatch.
+
+#include "cwd_sys.h"
+#include "test_support.h"
+
+#include <cstdlib>
+#include <sstream>
+#include <string>
+
+#include <libcwd/demangle.h>
+
+namespace {
+
+char const* test_cases[] = {
+  "_ZN12libcw_app_ct10add_optionIS_EEvMT_FvPKcES3_cS3_S3_",
+  "_ZGVN5libcw24_GLOBAL__N_cbll.cc0ZhUKa23compiler_bug_workaroundISt6vectorINS_13omanip_id_tctINS_5debug32memblk_types_manipulator_data_ctEEESaIS6_EEE3idsE",
+  "_ZN6libcwd13cwprint_usingIN5libcw9_private_12GlobalObjectEEENS_17cwprint_using_tctIT_EERKS5_MS5_KFvRSt7ostreamE",
+  "_ZNKSt14priority_queueIP27timer_event_request_base_ctSt5dequeIS1_SaIS1_EE13timer_greaterE3topEv",
+  "_ZNKSt15_Deque_iteratorIP15memory_block_stRKS1_PS2_EeqERKS5_",
+  "_ZNKSt17__normal_iteratorIPK6optionSt6vectorIS0_SaIS0_EEEmiERKS6_",
+  "_ZNSbIcSt11char_traitsIcEN6libcwd27no_alloc_checking_allocatorEE12_S_constructIPcEES5_T_S6_RKS2_",
+  "_Z1fI1APS0_PKS0_EvT_T0_T1_PA4_S3_M1CS8_",
+  "_Z3fooiPiPS_PS0_PS1_PS2_PS3_PS4_PS5_PS6_PS7_PS8_PS9_PSA_PSB_PSC_",
+  "_ZSt1BISt1DIP1ARKS2_PS3_ES0_IS2_RS2_PS2_ES2_ET0_T_SB_SA_PT1_",
+  "_ZngILi42EEvN1AIXplT_Li2EEE1TE",
+  "_X11TransParseAddress",
+  "_ZNSt13_Alloc_traitsISbIcSt18string_char_traitsIcEN6libcwd9_private_17allocator_adaptorIcSt24__default_alloc_templateILb0ELi327664EELb1EEEENS4_IS8_S6_Lb1EEEE15_S_instancelessE",
+  "_GLOBAL__I__Z2fnv",
+  "_Z1rM1GFivEMS_KFivES_M1HFivES1_4whatIKS_E5what2IS8_ES3_",
+  "_Z1xINiEE",
+  "_Z3fooIA6_KiEvA9_KT_rVPrS4_",
+  "_Z1fILi5E1AEvN1CIXqugtT_Li0ELi1ELi2EEE1qE",
+  "_Z1fILi5EEvN1AIXcvimlT_Li22EEE1qE",
+  "_Z1fPFYPFiiEiE",
+  "_Z1fI1XENT_1tES2_",
+  "_Z1fILi5E1AEvN1CIXstN1T1tEEXszsrS2_1tEE1qE",
+  "_Z1fILi1ELc120EEv1AIXplT_cviLd4028ae147ae147aeEEE",
+  "_Z1fILi1ELc120EEv1AIXplT_cviLf3f800000EEE",
+  "_Z9hairyfuncM1YKFPVPFrPA2_PM1XKFKPA3_ilEPcEiE",
+  "_Z4dep9ILi3EEvP3fooIXgtT_Li2EEE"
+};
+
+// Demangle every test case into captured, one result per line.
+//
+// Unknown or intentionally unsupported names are expected to be copied through by
+// demangle_symbol, so all cases are compared uniformly as text.
+void write_demangled_output(std::ostream& captured)
+{
+  std::string result;
+  for (char const* test_case : test_cases)
+  {
+    libcwd::demangle_symbol(test_case, result);
+    captured << result << '\n';
+    result.erase();
+  }
+}
+
+} // namespace
+
+int main()
+{
+  Debug(main_reached());
+  Debug(libcw_do.on());
+
+  std::stringstream captured;
+  write_demangled_output(captured);
+
+  char const* expected[] = {
+    "void libcw_app_ct::add_option<libcw_app_ct>(void (libcw_app_ct::*)(char const*), char const*, char, char const*, char const*)",
+    "guard variable for libcw::(anonymous namespace)::compiler_bug_workaround<std::vector<libcw::omanip_id_tct<libcw::debug::memblk_types_manipulator_data_ct>, std::allocator<libcw::omanip_id_tct<libcw::debug::memblk_types_manipulator_data_ct> > > >::ids",
+    "libcwd::cwprint_using_tct<libcw::_private_::GlobalObject> libcwd::cwprint_using<libcw::_private_::GlobalObject>(libcw::_private_::GlobalObject const&, void (libcw::_private_::GlobalObject::*)(std::ostream&) const)",
+    "std::priority_queue<timer_event_request_base_ct*, std::deque<timer_event_request_base_ct*, std::allocator<timer_event_request_base_ct*> >, timer_greater>::top() const",
+    "std::_Deque_iterator<memory_block_st*, memory_block_st* const&, memory_block_st* const*>::operator==(std::_Deque_iterator<memory_block_st*, memory_block_st* const&, memory_block_st* const*> const&) const",
+    "std::__normal_iterator<option const*, std::vector<option, std::allocator<option> > >::operator-(std::__normal_iterator<option const*, std::vector<option, std::allocator<option> > > const&) const",
+    "char* std::basic_string<char, std::char_traits<char>, libcwd::no_alloc_checking_allocator>::_S_construct<char*>(char*, char*, libcwd::no_alloc_checking_allocator const&)",
+    "void f<A, A*, A const*>(A, A*, A const*, A const* (*) [4], A const* (* C::*) [4])",
+    "foo(int, int*, int**, int***, int****, int*****, int******, int*******, int********, int*********, int**********, int***********, int************, int*************, int**************, int***************)",
+    "std::D<A*, A*&, A**> std::B<std::D<A*, A* const&, A* const*>, std::D<A*, A*&, A**>, A*>(std::D<A*, A* const&, A* const*>, std::D<A*, A* const&, A* const*>, std::D<A*, A*&, A**>, A**)",
+    "void operator-<42>(A<(42) + (2)>::T)",
+    "_X11TransParseAddress",
+    "std::_Alloc_traits<std::basic_string<char, std::string_char_traits<char>, libcwd::_private_::allocator_adaptor<char, std::__default_alloc_template<false, 327664>, true> >, libcwd::_private_::allocator_adaptor<std::basic_string<char, std::string_char_traits<char>, libcwd::_private_::allocator_adaptor<char, std::__default_alloc_template<false, 327664>, true> >, std::__default_alloc_template<false, 327664>, true> >::_S_instanceless",
+    "global constructors keyed to _Z2fnv",
+    "r(int (G::*)(), int (G::*)() const, G, int (H::*)(), int (G::*)(), what<G const>, what2<G const>, int (G::*)() const)",
+    "_Z1xINiEE",
+    "void foo<int const [6]>(int const [9][6], int const restrict (* volatile restrict) [9][6])",
+    "void f<5, A>(C<(((5) > (0))) ? (1) : (2)>::q)",
+    "void f<5>(A<(int)((5) * (22))>::q)",
+    "f(int (*(*) [extern \"C\"] (int))(int))",
+    "X::t f<X>(X::t)",
+    "void f<5, A>(C<sizeof (T::t), sizeof (T::t)>::q)",
+    "void f<1, (char)120>(A<(1) + ((int)((double)1.234e+1))>)",
+    "void f<1, (char)120>(A<(1) + ((int)((float)1))>)",
+    "hairyfunc(int (* const (X::** (* restrict (* volatile* (Y::*)(int) const)(char*)) [2])(long) const) [3])",
+    "void dep9<3>(foo<((3) > (2))>*)",
+    nullptr
+  };
+
+  return libcwd_ctest::matches_expected_output(captured, expected) ? EXIT_SUCCESS : EXIT_FAILURE;
+}
